3effe34bcb9b56eeeb025ef6e01d3e4e95b25c95
bishop255 <andresalejandro2001@gmail.com>
2025-11-29 17:33:28 -0300

    Create import.sql
    Se agregaron en el import.sql los productos de la tienda (falta agregar el de los carrusel

---
+++ src/main/resources/templates/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO products (name, description, price) VALUES ('Recarga Genshin Impact Genesis Crystals', '1980 Protogemas + 260 Bonus', 29990);
+INSERT INTO products (name, description, price) VALUES ('ELDEN RING Shadow of the Erdtree Premium Bundle (DLC)', 'Se requiere el juego base Elden Ring para poder jugar', 49990);
+INSERT INTO products (name, description, price) VALUES ('Overwatch® 2: Pink Mercy Skin (DLC)', 'Cosmetico para el personaje Mercy', 9990);
+INSERT INTO products (name, description, price) VALUES ('METAL GEAR SOLID Δ: SNAKE EATER', 'Juego Base', 32990);
+INSERT INTO products (name, description, price) VALUES ('Personal 5 Royal', 'Juego Base + Contenido Adicional', 149990);
+INSERT INTO products (name, description, price) VALUES ('Minecraft', 'Edition Bedrock + Java', 19990);
